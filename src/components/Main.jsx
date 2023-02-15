8e011050d0109398e48cfdf66d7335e40eda7858
Miguel <miguelfernando477@gmail.com>
2023-02-15 10:11:17 -0600

    switch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import dummyContacts from "../dummyData";
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        {
+          <table>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+              </tr>
+              {contacts.map((contact) => {
+                return 
+                  <tr>
+                    <td>{contact.name}</td>
+                    <td>{contact.phone}</td>
+                    <td>{contact.email}</td>
+                  </tr>
+                ;
+              })}
+            </tbody>
+          </table>
+        }
+      </div>
     </div>
   );
 };
